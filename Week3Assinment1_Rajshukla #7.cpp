5aafda27bb335a339cdc09e13171a305f000f584
Raj Shukla <rajshukla9630@gmail.com>
2017-07-13 22:14:13 +0530

    Update Week3Assinment1_Rajshukla #7.cpp

--- Week3Assinment1_Rajshukla #7.cpp
+++ Week3Assinment1_Rajshukla #7.cpp
@@ -1,60 +1,61 @@
 #include<iostream>
-using namespace std;
-int main()
-{
-   int arr[10],s,p,key,i,b=0,c=0,pos;
-   int found=0;
-   cout<<"enter size of array:";
-   cin>>s;
-   cout<<"enter array elements:";
-   for(i=0;i<s;i++)
-    {
-       cin>>arr[i];
+ using namespace std;
+
+ int binsearch(int arr[],int i,int j,int key)
+ {
+ 	while(j>=i)
+ 	{
+ 		int mid=(i+j)/2;
+ 		if(arr[mid]==key)
+ 		{
+ 		 cout<<"Element found at position:"<<mid<<endl;
+ 		 break;
+ 	    }
+ 		else if(arr[mid]>key)
+ 		j=mid-1;
+ 		else
+ 		i=mid+1;
+ 	}
+ 	if(i>j)
+ 	cout<<"element not available";
+ }
+ int search(int arr[],int i,int j,int key)
+ {
+ 	if(arr[i]>arr[j])
+ 	{
+ 		int mid=(i+j)/2;
+ 		if(arr[mid]==key)
+ 		{
+
+ 		    cout<<"available at location"<<mid;
+ 		    return 0;
+ 		}
+ 		if((arr[i]>arr[mid-1])&&((key>=arr[i])||(key<=arr[mid-1]))) search(arr,i,mid-1,key); //searching in rotated first group.
+ 	          else if((arr[mid+1]>arr[j])&&((key>=arr[mid+1])||(key<=arr[j]))) search(arr,mid+1,j,key); //searching in rotated second group.
+ 		else if((arr[i]<=arr[mid-1])&&((key<=arr[mid-1])&&(key>=arr[i]))) binsearch(arr,i,mid-1,key );// first group is sorted and element lies in it.
+ 		else if((arr[mid+1]<=arr[j])&&((key>=arr[mid+1])&&(key<=arr[j]))) binsearch(arr,mid+1,j,key); //second group is sorted and element lies in it.
+ 		else
+ 		{
+ 			cout<<"Not found";
+ 			return 0;
+ 		}
+
+ 	}
+ 	else binsearch(arr,i,j,key);
     }
+ int main()
+ {
+ 	int s,key;
+ 	cout<<"Enter array size:";
+	cin>>s;
+ 	int arr[s];
+ 	cout<<"Enter array elements: \n";
+ 	for(int i=0;i<s;i++)
+ 	{
+ 		cin>>arr[i];
+ 	}
+ 	cout<<"Enter element to be searched";
+ 	cin>>key;
+ 	search(arr,0,s-1,key);
+ }
 
-     cout<<"\n enter key:";
-     cin>>key;
-      for(i=0;i<s;i++)
-      {
-          if(arr[i+1]<arr[i])
-            p=arr[i];
-      }
-
-
-       for(i=0;i<p;i++)  //linear search for first group.
-           {
-             if(arr[i]==key)
-             {
-                 b=1;
-                 pos=i;
-                 break;
-             }
-           }
-
-           if(b==1)
-            {
-            cout<<"\n number found at position:"<<pos;
-            found++;
-            }
-
-         if(found==0)
-         {
-
-        for(i=p;i<s;i++)  //linear search in second group
-        {
-            if(arr[i]==key)
-            {
-                c=1;
-                pos=i;
-                break;
-            }
-        }
-        if(c==1)
-        {
-            cout<<"number found at position:"<<pos;
-        }
-         }
-    if(b==0&&c==0)
-    cout<<"not found";
-         return 0;
-}
